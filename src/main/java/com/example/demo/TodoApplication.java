4f86576bad6445fe163de1f0541b2b188f37c7ab
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-25 20:16:31 +0530

    refactor(core): change name of the file

--- src/main/java/com/example/demo/TodoApplication.java
+++ src/main/java/com/example/demo/TodoApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TakeHomeChallengeApplication {
+public class TodoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TakeHomeChallengeApplication.class, args);
+		SpringApplication.run(TodoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/demo/controller/AuthLogin.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+//spring framework
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class AuthLogin {
+
+ 
+}
