c2b9af3aa4c358d34bf6d345b1db8d8a5ad03325
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-28 12:40:01 +0530

    feat(LoginResponse): add unique key

--- src/main/java/com/example/demo/model/LoginResponse.java
+++ src/main/java/com/example/demo/model/LoginResponse.java
@@ -1,14 +1,24 @@
 package com.example.demo.model;
 
 public class LoginResponse {
+  private int id;
   private String message;
   private String accessToken;
    
+  public LoginResponse(int id, String message, String accessToken) {
+    this.id = id;
+    this.message = message;
+    this.accessToken = accessToken;
+  }
+
   public LoginResponse(String message, String accessToken) {
     this.message = message;
     this.accessToken = accessToken;
   }
 
+  public int getId() {
+    return id;
+  }
   public String getMessage() {
     return message;
   }
