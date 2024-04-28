e103fb28dbd59f88085ab4bf8782a8d709366ba2
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-28 12:39:28 +0530

    feat(LoginRepo): remove unwanted logic

--- src/main/java/com/example/demo/repo/LoginRepo.java
+++ src/main/java/com/example/demo/repo/LoginRepo.java
@@ -1,16 +1,11 @@
 package com.example.demo.repo;
 //springframework
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 //model
 import com.example.demo.model.LoginModel;
 
 @Repository
   public interface LoginRepo extends JpaRepository<LoginModel, Integer> {
-    LoginModel findByUsernameAndPassword(String userName,  String password);
-    @Query("SELECT l.token FROM LoginModel l WHERE l.username = ?1 AND l.password = ?2")
-    String findTokenByUsernameAndPassword(String userName, String password);
-  
+    LoginModel findByUsernameAndPassword(String userName,  String password); 
   } 
-
