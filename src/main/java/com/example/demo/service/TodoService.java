fc44869b7bfb34cca2ed7691367fd31fe61d1d9c
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-29 01:31:43 +0530

    feat(TodoService): add logic for get all method in service

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,12 +1,16 @@
 package com.example.demo.service;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 //service
 import org.springframework.stereotype.Service;
 ///model
 import com.example.demo.model.LoginModel;
 import com.example.demo.model.LoginResponse;
+import com.example.demo.model.ProjectInfo;
 //repository
 import com.example.demo.repo.LoginRepo;
+import com.example.demo.repo.ProjectRepo;
 //interface
 import com.example.demo.security.TodoInterface;
 
@@ -15,6 +19,8 @@ public class TodoService implements TodoInterface {
 
 @Autowired
 private LoginRepo loginService;
+@Autowired
+private ProjectRepo projectService;
 
   public LoginResponse checkLogin(String userName, String password) {
     LoginModel user = loginService.findByUsernameAndPassword(userName, password);
@@ -24,5 +30,9 @@ private LoginRepo loginService;
       String accessToken = user.getToken();
       return new LoginResponse(user.getId(),"Logged in successfully", accessToken);
     }
+  }
+
+  public List<ProjectInfo> getProjectDetails(long userId) {
+    return projectService.findByUser(userId);
   }
 }
