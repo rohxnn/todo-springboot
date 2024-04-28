ee95069b9259bcebd2f41d84828f1b11e952743b
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-29 01:30:36 +0530

    refator(ProjectRepo): change query

--- src/main/java/com/example/demo/repo/ProjectRepo.java
+++ src/main/java/com/example/demo/repo/ProjectRepo.java
@@ -3,12 +3,14 @@ package com.example.demo.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 //model
-import com.example.demo.model.LoginModel;
 import com.example.demo.model.ProjectInfo;
 
 @Repository
 public interface ProjectRepo extends JpaRepository<ProjectInfo, Long> {
-List<ProjectInfo> findByUser(LoginModel l);
+  @Query("SELECT NEW com.example.demo.model.ProjectInfo(p.id, p.title, p.status, p.description, p.createdDate, p.updatedDate) FROM ProjectInfo p INNER JOIN LoginModel l ON p.id = l.id WHERE l.id = :userId")
+  List<ProjectInfo> findByUser(Long userId);
 }
