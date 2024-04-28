8083179466a01857f2e7f7909a6dddd11ba26cc9
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-04-29 01:31:18 +0530

    feat(TodoInterface): add get all method in interface

--- src/main/java/com/example/demo/security/TodoInterface.java
+++ src/main/java/com/example/demo/security/TodoInterface.java
@@ -1,7 +1,11 @@
 package com.example.demo.security;
 
+import java.util.List;
+
 import com.example.demo.model.LoginResponse;
+import com.example.demo.model.ProjectInfo;
 
 public interface TodoInterface {
   public LoginResponse checkLogin(String userName, String password);
+  public List<ProjectInfo> getProjectDetails(long userId);
 }
